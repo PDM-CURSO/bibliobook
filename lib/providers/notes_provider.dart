73d20a8c1092aab2de118d88f3a9588f3a126e33
01fc <javier.francisco01.09@gmail,com>
2023-04-17 22:29:05 -0600

    todos agregados

--- lib/providers/notes_provider.dart
+++ lib/providers/notes_provider.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
 class NotesProvider with ChangeNotifier {}
+// TODO: convertir los otros provider en singleton
+// TODO: implemente metodo para borrar formulario
+// TODO: implemente metodo para borrar archivo de audio
+// TODO: implementar el guardado de datos en Cloud Firestore
+// TODO: implementar guardado de imagenes/audios en Cloud Storage
+// TODO: cambie el app icon a un icono mas bonito
+// TODO: renombre la app
